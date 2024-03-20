2df2d016184d3ddf6bf999e39b93cb46971460c5
Krhitlashi <krhitlashi@outlook.com>
2024-03-20 14:17:23 -0700

    ſ̀ȷᴜ ſɭᴜꞇ

--- j͑ʃɹ ſɭɹ ſןɹ }ʃꞇ/j͑ʃᴜ ı],ɔ.gd
+++
@@ -1,7 +0,0 @@
-extends Area3D
-
-@export_dir var ហ្វ៍សាជេ : String
-
-func កេតេមិ():
-	Kiitse.ហ្វ៍តេមិ.ហ្វ៍សាជេ = ហ្វ៍សាជេ
-	get_tree().root.add_child(Kiitse.ហ្វ៍តេមិ)
--- j͑ʃɹ ſɭɹ ſןɹ }ʃꞇ/ſɭɹ ſᶘɔ.gd
+++ j͑ʃɹ ſɭɹ ſןɹ }ʃꞇ/ſɭɹ ſᶘɔ.gd
@@ -1,3 +1,1 @@
 extends Node
-
-var ហ្វ៍តេមិ = preload("res://ʃэ ɭʃɔ ŋᷠɹ.tscn").instantiate()
