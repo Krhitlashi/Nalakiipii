1ca9613f796fc421206e90c5d2d09d64ac98bbaf
Krhitlashi <krhitlashi@outlook.com>
2024-03-28 20:43:36 -0700

    ſ̀ȷᴜ ſɭᴜꞇ ſɟᴜ ſᶘɹ }ʃᴜ

--- j͑ʃɹ ſɭɹ ſןɹ }ʃꞇ/}ʃᴜ ŋᷠᴜ ꞁȷ̀ɜ ſᶘɹ }ʃᴜ.gd
+++ j͑ʃɹ ſɭɹ ſןɹ }ʃꞇ/}ʃᴜ ŋᷠᴜ ꞁȷ̀ɜ ſᶘɹ }ʃᴜ.gd
@@ -1,5 +1,13 @@
 extends Control
 
 func _ready():
-	if !DisplayServer.is_touchscreen_available():
+	if DisplayServer.is_touchscreen_available():
 		hide()
+
+func កុផុយ():
+	if 	$"../ſןw".visible:
+		$"../ſןw".hide()
+		$"../֭ſɭɔⅎ ɭʃᴜƴ".hide()
+	else:
+		$"../ſןw".show()
+		$"../֭ſɭɔⅎ ɭʃᴜƴ".show()
