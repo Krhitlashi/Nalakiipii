0650ab66e11aa29af423ccfc0eec1ec29438ef1d
Krhitlashi <krhitlashi@outlook.com>
2024-05-26 12:34:55 -0700

    ſ̀ȷᴜ ſɭᴜꞇ ꞁȷ̀ᴜ ɽ͑ʃ'ᴜȝ

--- ı],ᴜ j͑ʃɔ ı],ᴜƴ.gd
+++ ı],ᴜ j͑ʃɔ ı],ᴜƴ.gd
@@ -1,5 +1,5 @@
 extends Control
 
 func _input(event):
-	if event is InputEventScreenDrag || (event is InputEventMouse && InputEventMouseMotion):
+	if event is InputEventScreenDrag || (event is InputEventMouseButton && InputEventMouseMotion):
 		position = event.get_position() - get_viewport_rect().size / 2
