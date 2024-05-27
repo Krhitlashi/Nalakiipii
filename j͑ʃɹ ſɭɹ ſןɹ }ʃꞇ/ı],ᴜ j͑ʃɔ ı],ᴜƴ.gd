74818daeeab6dfd9db60d1ce0070e481c4442f76
Krhitlashi <krhitlashi@outlook.com>
2024-05-27 12:17:35 -0700

    ſ̀ȷᴜ ſɭᴜꞇ

--- j͑ʃɹ ſɭɹ ſןɹ }ʃꞇ/ı],ᴜ j͑ʃɔ ı],ᴜƴ.gd
+++ j͑ʃɹ ſɭɹ ſןɹ }ʃꞇ/ı],ᴜ j͑ʃɔ ı],ᴜƴ.gd
@@ -1,5 +1,5 @@
 extends Control
 
 func _input(event):
-	if event is InputEventScreenDrag:
+	if event is InputEventScreenDrag || (Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && !Input.is_action_just_pressed("ſɟɹᶗ‹")) :
 		position = event.get_position() - get_viewport_rect().size / 2
