e3e4ffcd7044c40f3979a0b9d677eff2a97d3610
Krhitlashi <krhitlashi@outlook.com>
2024-05-27 21:22:00 -0700

    ſ̀ȷᴜ ſɭᴜꞇ

--- j͑ʃɹ ſɭɹ ſןɹ }ʃꞇ/ŋᷠᴜ ſɭᴜ ŋᷠᴜ.gd
+++ j͑ʃɹ ſɭɹ ſןɹ }ʃꞇ/ŋᷠᴜ ſɭᴜ ŋᷠᴜ.gd
@@ -5,14 +5,18 @@ const ពពឺ = 4.8
 
 var ក្នាហេំតារ = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var លារ = $"j͐ʃᴜƴ"
-@onready var ងេនា = $"j͐ʃᴜƴ/Camera3D"
+@onready var ងេនា = $"j͐ʃᴜƴ/ꞁȷ̀ɹ ʃᴜ }ʃɹ"
+@onready var ហ្រិត្លឺ = $"ſɭ,ɹ ſ̀ȷw"
+@onready var ពាត៏ = $"ſɭ,ɹ ſ̀ȷw/ſןᴜ ɭʃэ"
+@onready var តាម្ចេ = $"ɭʃᴜc̭ ſɟɔ"
+@onready var តាមា = $"ɭʃᴜc̭ ſɟɔ/ɭʃᴜ ŋᷠᴜ"
 
 # ſןw
 func ពឺ():
 	if Input.is_action_just_pressed("ſןw") && (is_on_floor() || Kiitse.ហ្តេយាង្យុ == true):
 		velocity.y = ពពឺ
-	if Kiitse.ងៃពឹ == true && is_on_floor():
-		velocity.y = 2
+	if Kiitse.ងៃពឹ == true:
+		velocity.y = ពពឺ
 	if Input.is_action_just_pressed("֭ſɭɔⅎ ɭʃᴜƴ") && (!is_on_floor() || Kiitse.ហ្តេយាង្យុ == true):
 		velocity.y = -ពពឺ
 		
@@ -28,18 +32,25 @@ func ជា():
 		velocity.z = move_toward(velocity.z, 0, ពសេហេ)
 
 func _unhandled_input(event):
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton && !get_tree().current_scene.scene_file_path == "res://j͑ʃɔ ı],ꞇ.tscn":
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif event.is_action_released("ui_cancel") || get_tree().current_scene.scene_file_path == "res://j͑ʃɔ ı],ꞇ.tscn":
+	elif event.is_action_released("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			លារ.rotate_y(-event.relative.x * 0.01)
 			ងេនា.rotate_x(-event.relative.y * 0.01)
+			ហ្រិត្លឺ.rotate_y(-event.relative.x * 0.01)
+			ពាត៏.rotate_x(-event.relative.y * 0.01)
+			តាម្ចេ.rotate_y(-event.relative.x * 0.01)
+			តាមា.rotate_x(-event.relative.y * 0.01)
 	if event is InputEventScreenDrag:
 		លារ.rotate_y(-event.relative.x * 0.01)
 		ងេនា.rotate_x(-event.relative.y * 0.01)
-			
+		ហ្រិត្លឺ.rotate_y(-event.relative.x * 0.01)
+		ពាត៏.rotate_x(-event.relative.y * 0.01)
+		ហ្រិត្លឺ.rotate_y(-event.relative.x * 0.01)
+		តាមា.rotate_x(-event.relative.y * 0.01)
 func _physics_process(delta):
 	# ſɭc̗ᴜ ֭ſɭɔⅎ ɭʃᴜƴ
 	if !is_on_floor() && Kiitse.ហ្តេយាង្យុ == false && !get_tree().current_scene.scene_file_path == "res://ꞁȷ̀ᴜ ɽ͑ʃᴜȝ/j͑ʃɔ ı],ᴜƴ.tscn":
