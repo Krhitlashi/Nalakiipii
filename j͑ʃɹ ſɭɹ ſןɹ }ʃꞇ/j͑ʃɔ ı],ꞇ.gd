f08918517a84e1d5980bf5a1efe7f6dc461031eb
Krhitlashi <krhitlashi@outlook.com>
2024-05-28 21:27:05 -0700

    ſ̀ȷᴜ ſɭᴜꞇ

--- j͑ʃɹ ſɭɹ ſןɹ }ʃꞇ/j͑ʃɔ ı],ꞇ.gd
+++ j͑ʃɹ ſɭɹ ſןɹ }ʃꞇ/j͑ʃɔ ı],ꞇ.gd
@@ -1,6 +1,10 @@
-extends Button
+extends TextureButton
 
 @export var អារាង : PackedScene = load("res://ꞁȷ̀ᴜ ɽ͑ʃᴜȝ/ſ͔ɭɹƴ ſ͔ɭɹ.tscn")
+@export var ផាលិជា : Popup
 
 func ខិរខិ():
 	Kootlai.ក្សីរិ(អារាង)
+
+func អំផាលិជា():
+	ផាលិជា.popup()
